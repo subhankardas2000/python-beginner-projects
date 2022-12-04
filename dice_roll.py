366d3a88523a722b7803433f59b9f9e61c2c0eb4
Subhankar Das <94730919+SubhankarDas2000@users.noreply.github.com>
2022-12-04 15:25:51 +0530

    Update dice_roll.py

--- dice_roll.py
+++ dice_roll.py
@@ -1,13 +1,3 @@
-#How it works
-#This is a classic “roll the dice” program.
-#We will be using the random module for this,since we want to randomize the numberswe get from the dice.
-
-#We set two variables (min and max) , lowest and highest number of the dice.
-#We then use a while loop, so that the user can roll the dice again.
-
-#The roll_again can be set to any value, but here it’s set to “yes” or “y”,
-#but you can also add other variations to it.
-
 #Rolling the dice
 
 import random
